47f7770c50fa4efde10c84d246b346a8e557ae52
Mueeza-517 <mueezaakbar517@gmail.com>
2024-09-15 21:00:43 +0500

    message

---
+++ task8.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+main()
+{
+system("color 7D");
+cout<<"                                  oooooo       oooooo                                     "<<endl;
+cout<<" ........                        o      o     o      o                ...........         "<<endl;
+cout<<" .........                           ooo      o      o                ..........          "<<endl;
+cout<<" ..........                       oo          o      o                .........           "<<endl;
+cout<<" ...........                    oooooooo       oooooo     th          ........             "<<endl;
+cout<<"   ..........           ----------------------------------------      .......              "<<endl;
+cout<<"     .........            C      E     N     T     U    R     Y       ......               "<<endl;
+cout<<"      .........        ----------------------------------------       .....                "<<endl;
+cout<<"         .......           ooooooo  ooooooo    o     o  ooooooo       ....                 "<<endl;
+cout<<"           .....              o     o           o   o      o          ...                  "<<endl;
+cout<<"              ...             o     ooooo         o        o          ..                   "<<endl;
+cout<<"                ..            o     o           o   o      o          ..                   "<<endl;
+cout<<"             ___||___         o     ooooooo    o     o     o       ___||___                "<<endl;
+cout<<"  __________|________|_____                             __________|        |_______________"<<endl;
+}
+                      
+        
---
+++ task9.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+main()
+{
+cout<<"   -------------------------------------------------    "<<endl;
+cout<<"  o      ^     ^                                        "<<endl;
+cout<<"          -----                                         "<<endl;
+cout<<"     o    (o o)\\____________                           "<<endl;
+cout<<"          (___)\\            )\\/\\                     "<<endl;
+cout<<"               ||--------W  |                           "<<endl;
+cout<<"               ||           ||                          "<<endl;
+}
