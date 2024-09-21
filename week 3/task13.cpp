47dc17b2788c1dfa6071e8f2fa4461f6ccdac759
Mueeza-517 <mueezaakbar517@gmail.com>
2024-09-21 12:25:23 +0500

    Add files via upload

---
+++ week 3/task13.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+main()
+{
+int number;
+cout<<"enter first number:";
+cin >> number;
+int sum;
+sum=number;
+cout <<"enter second number:";
+cin >>number;
+sum=sum+number;
+cout <<"enter third number:";
+cin >>number;
+sum=sum+number;
+cout<<"enter fourth number:";
+cin >>number;
+sum=sum+number;
+cout <<"enter fifth number:";
+cin >>number;
+sum=sum+number;
+cout <<"sum is:"<<sum;
+}
